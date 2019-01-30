a854e687a9bacf4000ca6cd7292a56940798b42b
parthparmar7 <parthparmar7@gmail.com>
2019-01-30 14:47:48 +0530

    initial commit

---
+++ db.js
@@ -0,0 +1,9 @@
+/*var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://parth:parth@cluster0-shard-00-00-p5exz.mongodb.net:27017,cluster0-shard-00-01-p5exz.mongodb.net:27017,cluster0-shard-00-02-p5exz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});*/
+
